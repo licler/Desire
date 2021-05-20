e35968ef7ebee543a9a881dfda53caefc534fd96
licler <ute3@yandex.ru>
2021-05-20 21:52:54 +0300

    right btn

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,8 +8,15 @@ $(function(){
     autoplay: true
   });
 	
-	
-	
+
+  $('.header__btn').on('click', function(){
+    $('.right-btn').removeClass('right-btn--close');
+  });
+  $('.right-btn__close').on('click', function(){
+    $('.right-btn').addClass('right-btn--close');
+  });
+
+
   var mixer = mixitup('.furniture__items',{
     load: {
         filter: '.category-b'
