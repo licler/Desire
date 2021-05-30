8fbfddc4a65208fb3bc65470bfa09463a5f776ca
licler <ute3@yandex.ru>
2021-05-30 19:44:27 +0300

    start blog

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,6 +7,12 @@ $(function(){
 	  fade: true,
     autoplay: true
   });
+
+  // $('.blog__table-items').slick({	
+  //   arrows: true,
+  //   prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/content/blog/back.svg" alt="images"></button>',
+  //   nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/content/blog/next.svg" alt="images"></button>'
+  // });
 	
 
   $('.header__btn').on('click', function(){
